1b24b206b9b02452c0e910f3a45b1e7ae5f2bad9
Rebecca Thomas <rthomas@hmc.edu>
2013-05-03 13:03:04 -0700

    calculating rays and anchor points

--- cartoon.py
+++ cartoon.py
@@ -2,7 +2,7 @@ import sys
 import pysvg.parser
 from pysvg.shape import *
 from pysvg.builders import *
-
+from geometry import *
 
 def readSVG(fname):
     return pysvg.parser.parse(fname)
@@ -29,17 +29,29 @@ def readInput(filename):
     cameraPos = []
     for i in range(0, numViews):
         keyViews.append(readSVG(f.readline()[:-1]))
-        cameraPos.append([float(f.readline()), 
+        cameraPos.append(Point(float(f.readline()), 
                           float(f.readline()), 
-                          float(f.readline())])
+                          float(f.readline())))
     return numViews, keyViews, cameraPos
 
 def calcRays(keyViews, cameraPos, stroke):
-    pos = (100, 120)
+    pos = Point(100, 120, 0)
     rays = []
-#    for i in range(len(keyViews)):
-        
-
+    for i in range(len(keyViews)):
+        rays.append(Ray(pos, Vector(pos, cameraPos[i])))
+    return rays
+    
+def calcAnchorPos(keyViews, cameraPos, stroke):
+    rays = calcRays(keyViews, cameraPos, stroke)
+    anchorPos = Point(0, 0, 0)
+    for i in range(0, 10):
+        tempPos = Point(0, 0, 0)
+        for r in rays:
+            tempVec = Vector(r.pos, anchorPos)
+            tempPos += r * (tempVec.dot(r.dir) / r.dir.dot(r.dir))
+        anchorPos = tempPos/3
+    return anchorPos
+    
 if __name__ == '__main__':
     inputFile = sys.argv[1]
     numKeyViews, keyViews, cameraPos = readInput(inputFile)
@@ -60,5 +72,13 @@ if __name__ == '__main__':
 
     svg = keyViews[0]
     
+    strokes = []
+    for stroke in keyDicts[0].keys():
+        strokes.append(stroke)
+  
+    anchorPos = []
+    for stroke in strokes:
+        anchorPos.append(calcAnchorPos(keyDicts, cameraPos, stroke))
 
+    print anchorPos
     writeSVG(outputFile, svg)
---
+++ geometry.py
@@ -0,0 +1,50 @@
+class Point(object):
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x, self.y, self.z = x, y, z
+
+    def __repr__(self):
+        return 'Point(x=%s, y=%s, z=%s)' % (self.x, self.y, self.z)
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __div__(self, num):
+        return Point(self.x / num, self.y / num, self.z / num)
+
+class Vector(object):
+    
+    def __init__(self, start, end):
+        self.x = end.x - start.x
+        self.y = end.y - start.y
+        self.z = end.z - start.z
+
+    def __repr__(self):
+        return 'Point(x=%s, y=%s, z=%s)' % (self.x, self.y, self.z)
+    
+    def dot(self, toDot):
+        dotProduct = self.x * toDot.x
+        dotProduct += self.y * toDot.y
+        dotProduct += self.z * toDot.z
+        return dotProduct
+
+    def cross(self, toCross):
+        newX = self.y * toCross.z - self.z * toCross.y
+        newY = self.z * toCross.x - self.x * toCross.z
+        newZ = self.x * toCross.y - self.y * toCross.x
+        return Vector(newX, newY, newZ)
+
+    def __mul__(self, num):
+        return Vector(Point(self.x, self.y, self.z), 
+                      Point(self.x * num, self.y * num, self.z * num))
+
+class Ray(object):
+    
+    def __init__(self, pos, dir):
+        self.pos, self.dir = pos, dir
+    
+    def __repr__(self):
+        return 'Pos:%s Dir:%s' % (self.pos, self.dir)
+
+    def __mul__(self, num):
+        return self.pos + self.dir * num
