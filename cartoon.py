2c64a79021963c58d2abaa6868f4e89a68c4f266
Rebecca Thomas <rthomas@hmc.edu>
2013-05-05 23:58:12 -0700

    partial mirroring
    have it mirroring but the offset isn't right. only getting a quarter of
    the image.

--- cartoon.py
+++ cartoon.py
@@ -9,13 +9,32 @@ from cartoonCalc import *
 if __name__ == '__main__':
     inputFile = sys.argv[1]
     numKeyViews, keyViews, cameraPos = readInput(inputFile)
+
+    newSvg = pysvg.parser.svg()
+    viewBox = keyViews[0].get_viewBox().split(' ')
+    newViewBox = [float(viewBox[2]) * -.5, float(viewBox[3]) * -.5, 
+                  float(viewBox[2]) * .5, float(viewBox[3]) * .5]
     
+    offsetMatrix = TransformBuilder()
+    offsetMatrix.setMatrix(1, 0, 0, 1, newViewBox[0], newViewBox[1])
+
+
     reverseMatrix = TransformBuilder()
-    reverseMatrix.setRotation(90)
-    #reverseMatrix.setMatrix(-1, 0, 0, 1, 0, 0) 
+   # reverseMatrix.setScaling(1, 5)
+    reverseMatrix.setMatrix(-1, 0, 0, 1, newViewBox[0] *-1, newViewBox[1]) 
+
+#    for i in range(0, len(newViewBox)):
+#        newViewBox[i] = 2 * newViewBox[i]
+
+    print newViewBox
+    newViewBox = ' '.join(map(str, newViewBox))
+    print newViewBox
+    newSvg.set_viewBox(newViewBox)
+
     for i in range(0, numKeyViews):
+        keyViews[i].set_transform(offsetMatrix.getTransform())
         keyViews.append(copy.deepcopy(keyViews[i]))
-        keyViews[i + numKeyViews].set_transform(reverseMatrix)
+        keyViews[i + numKeyViews].set_transform(reverseMatrix.getTransform())
         cameraPos.append(cameraPos[i] * -1)
     numKeyViews *= 2
 
@@ -35,10 +54,10 @@ if __name__ == '__main__':
 #        print dict
         keyDicts.append(dict)
 
-    print
-    print keyDicts[0][u'mouth']._attributes
-    print
-    print keyDicts[3][u'mouth']._attributes
+    #print
+    #print keyDicts[0][u'mouth']._attributes
+    #print
+    #print keyDicts[3][u'mouth']._attributes
         
 #    print keyDicts[1][u'leftEye'].getTopRight()
 
@@ -55,7 +74,7 @@ if __name__ == '__main__':
 
 #    print anchorPos
 
-    newSvg = pysvg.parser.svg()
+
 #    print keyDicts[4][strokes[0]]._attributes['fill'] 
 #    keyDicts[0][u'mouth']._attributes['stroke'] = unicode('#ffff00')
 
@@ -73,7 +92,7 @@ if __name__ == '__main__':
             newD = combineD(dList, weights)
             tempStroke = copy.deepcopy(keyDicts[viewNum][stroke])
             tempStroke.set_d(newD)
-            print keyViews[viewNum].get_transform().transform_dict
+#            print keyViews[viewNum].get_transform()
             tempStroke.set_transform(keyViews[viewNum].get_transform())
             newSvg.addElement(tempStroke)
     
